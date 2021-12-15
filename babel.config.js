4e1cb56013d6da4c316990f25e4184cd4d5f6f7b
unknown <agustinm06@gmail.com>
2021-12-14 21:38:29 -0300

    Agrego configuracion inicial eslint/prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@screens': './src/presentation/screens',
+            '@components': './src/presentation/components',
+            '@presentation': './src/presentation',
+            '@app': './src/app',
+            '@infraestructure': './src/infraestructure/',
+          },
+          extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.jsx', '.js', '.json'],
+        },
+      ],
+    ],
+  }
+}
